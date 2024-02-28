7516361ade4d4faef6a084ca047819e382fd148e
Samrudhi2711 <samgadge27@gmail.com>
2024-02-28 22:36:10 +0530

    built in fun  slicingTuple create tuple file added

---
+++ createTup.py
@@ -0,0 +1,11 @@
+#creating tuple
+tuple=(10,20,30,40,"sam",5441,17.28)
+tuple1=(2.44,89,29.378,"prachi","Salon",50,100,20000)
+tuple2=(tuple,tuple1,44444,5555,666,77777,333.333)
+print("print tuple :",tuple)
+print("print tuple2 :",tuple1)
+print("print tuple3 :",tuple2)
+
+#print tuple type
+print("Type of tuple :",type(tuple))
+print("Type of tuple1:",type(tuple1))
