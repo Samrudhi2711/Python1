bac066c6afee5114b5788a6d50a80715bf077ead
Samrudhi2711 <samgadge27@gmail.com>
2024-03-03 01:13:41 +0530

    Function1 file added

---
+++ funAllOper.py
@@ -0,0 +1,13 @@
+def sequence_operation(a):
+    print("Sequence element are:",a)
+    print("its type is :",type(a))
+    print("Max element is:",max(a))
+    print("min element is:",min(a))
+    print("sum of all ement is:",sum(a))
+list=[10,20,30,40,50,60,70,80,90]
+tuple=(1,2,3,4,5,6,7,8,9)
+set={11,22,33,44,55,66,77,88}
+
+sequence_operation(list)
+sequence_operation(tuple)
+sequence_operation(set)
