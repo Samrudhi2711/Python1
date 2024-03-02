bbe07c325ccb2b282efd2a5ec5a44fe6c9a0eb59
Samrudhi2711 <samgadge27@gmail.com>
2024-03-03 01:14:51 +0530

    parameter file added

---
+++ funPara.py
@@ -0,0 +1,6 @@
+def sample_function(para1,para2,para3):
+    print("print parameter one:",para1)
+    print("print parameter two:",para2)
+    print("print parameter three:",para3)
+    
+sample_function(20,23.45,89)
