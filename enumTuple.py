e2ba9ce1fe9128cf7106f248e258adb596b3ee6e
Samrudhi2711 <samgadge27@gmail.com>
2024-03-09 17:41:48 +0530

    enumTuple.py file added

---
+++ enumTuple.py
@@ -0,0 +1,8 @@
+# fruit=("Apple","Banana","Watermalon","pomegranet","chikoo","grapes","mango")
+# for index,name in enumerate(fruit):
+#     print(index,name)
+    
+fruit=("Apple","Banana","Watermalon","pomegranet","chikoo","grapes","mango")
+for index,name in enumerate(fruit, start=1):
+    print(index,name)
+    
