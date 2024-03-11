c986800128ee1bfa08cb7617cb3d1e46286475e2
Samrudhi2711 <samgadge27@gmail.com>
2024-03-11 21:48:14 +0530

    importMethod file added successfuly

---
+++ importMethod.py
@@ -0,0 +1,20 @@
+import math
+result=math.sqrt(9)
+print(result)
+
+from math import sqrt,pi
+result=math.sqrt(16)
+print(result)
+print(pi)
+
+#import everything using *
+
+from math import *
+print(sqrt(25))
+print(pi)
+
+#rename import module
+import math as m
+result=(m.sqrt(64))
+print(result)
+print(m.pi)
