efb2b88a038779b53c082c56d37d5b76bc9ce410
Samrudhi2711 <samgadge27@gmail.com>
2024-03-11 21:51:13 +0530

    dictCreate file added

---
+++ dictCreate.py
@@ -0,0 +1,17 @@
+#create dictionary
+#create empty 
+dict={}
+
+#create dict with int key
+dict1={1:"apple",2:"watermelon"}
+
+#create dict with  mix key 
+dict3={"name":"Samrudhi",2:"watermelon", 3:[1,2,3,4]}
+
+#dict4=dict([(1,"apple"),(2,"Ball")],)
+
+
+dict5={1:"sam", 2:"sal",3:"prachi",4:"vaishu"}
+print("At index '1',element is:",dict5[1])
+print("At index '2',element is:",dict5[2])
+print("At index '4',element is:",dict5[4])
