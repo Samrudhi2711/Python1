bbd48a2366851e8fb6e8d31c777ea414f4464c45
Samrudhi2711 <samgadge27@gmail.com>
2024-03-11 21:54:42 +0530

    Dict using looping file added

---
+++ loopusingDict.py
@@ -0,0 +1,15 @@
+# dict={"Ravi":"27/11/2003","sal":"02/05/2003","prachi":"03/04/2003","vaishu":"24/12/2003"}
+# for i in dict:
+#     print(i)
+
+
+# dict={"Ravi":"27/11/2003","sal":"02/05/2003","prachi":"03/04/2003","vaishu":"24/12/2003"}
+# for i in dict:
+#     print("At key",i,"value is:",dict[i])
+
+# dict1={"rollno":"10","name":"Samrudhi","marks":"90"}
+# print("the key() function will return:",dict1.keys())
+# print("the value() function will return:",dict1.values())
+
+
+    
