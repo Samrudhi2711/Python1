0f61f7850807b6b4a4dae6086957dd4127087f87
Samrudhi2711 <samgadge27@gmail.com>
2024-04-04 23:29:08 +0530

    classobject file added successfuly

---
+++ classObject.py
@@ -0,0 +1,8 @@
+class Details:
+    name="Sakshi"
+    age=21
+    marks=98
+    Occupation="software developer"
+
+obj1=Details()
+print("Name is",obj1.name ,"age is",obj1.age,"Marks is",obj1.marks,"Occupation is",obj1.Occupation)
