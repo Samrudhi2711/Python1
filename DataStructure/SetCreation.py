5406cabd366de36f355d2fc3379ae257a102f727
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 23:36:49 +0530

    Update SetCreation.py

--- DataStructure/SetCreation.py
+++ DataStructure/SetCreation.py
@@ -1,1 +1,12 @@
+#create set
+set1={10,20,30}
+print(set1)
+
+set2={21,31,42,"sam","saloni",83,80}
+print(set2)
+print(type(set1))
+
+#mix data
+set3={300,4000,56,"Samrudhi",(1,2,3,4)}
+print(type(set3))
 
