03cbf1b6a44211371de1e57ef84d755254e7f637
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 22:22:40 +0530

    Update TupleUsingLoop.py

--- DataStructure/TupleUsingLoop.py
+++ DataStructure/TupleUsingLoop.py
@@ -1,1 +1,18 @@
 
+# tuple1=(10,20,30,40,50,458.55,"Sam",15.71,90.23,23,12,44)
+# i=0
+# while i<tuple1.__len__():
+#     print("At index",i,"element is:",tuple1[i])
+#     i=i+1
+
+# tuple2=(30,40,53,42,67,87,42,90,80,70,300,200,100,"sam","sal","shra")
+# for ele in tuple2:
+#     print(ele)    
+    
+tuple3=(20,30,40,60,12.12,34.34,54.13)
+print("At index 1 we have:",tuple3[1])
+print("At index 3 we have:",tuple3[3])
+print("At index -2 we have:",tuple3[-2])
+print("At index 5 we have:",tuple3[5])
+print("At index -1 we have:",tuple3[-1])
+
