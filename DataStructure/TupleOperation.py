cf66a407e914d4be1a6e314a638a6e144cb48bdc
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 22:10:57 +0530

    Update TupleOperation.py

--- DataStructure/TupleOperation.py
+++ DataStructure/TupleOperation.py
@@ -1,1 +1,18 @@
 
+tuple=(10,20,30,"sam",65,"saloni",30,30,30,67,30,30,89,30,30,90,30,56,30,30,30,30,45)
+
+#count()
+a=tuple.count(30)
+print("total no of count 30:",a)
+
+#__add__
+tuple2=tuple.__add__((999999,900000,8000))
+print("adding element in tuple :",tuple2)
+
+#index(30)
+b=tuple2.index(30)
+print("first occurrence of 30 in tuple2 is at index:",b)
+
+#del
+#del tuple2
+
