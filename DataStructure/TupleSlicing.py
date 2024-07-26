857169cb8b2f603ca5ac67d66c05848ecfab568d
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 22:30:57 +0530

    Update TupleSlicing.py

--- DataStructure/TupleSlicing.py
+++ DataStructure/TupleSlicing.py
@@ -1,1 +1,23 @@
 
+tuple=("S","A","M","R","U","D","H","I","J","A","N","A","R","D","H","A","N","G","A","D","G","E")
+print("Original Tuple")
+print(tuple)
+
+print("Slicingb element in a range 3-8:")
+print(tuple[3:8])
+
+print("print Slicingb element from 5th index till the end:")
+print(tuple[5:])
+
+print("Printing all element:")
+print(tuple[:])
+
+print("Printing slice element upto -5 index")
+print(tuple[:-5])
+
+print("Printing slice element upto 5 index")
+print(tuple[:5])
+
+print("Printing all element in reverse order:")
+print(tuple[::-1])
+
