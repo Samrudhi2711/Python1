5738b40e5995312d222bb6ad2df3ded02e75e14c
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 22:35:25 +0530

    Update TupleEnum.py

--- DataStructure/TupleEnum.py
+++ DataStructure/TupleEnum.py
@@ -1,1 +1,10 @@
 
+# fruit=("Apple","Banana","Watermalon","pomegranet","chikoo","grapes","mango")
+# for index,name in enumerate(fruit):
+#     print(index,name)
+    
+fruit=("Apple","Banana","Watermalon","pomegranet","chikoo","grapes","mango")
+for index,name in enumerate(fruit, start=1):
+    print(index,name)
+    
+
