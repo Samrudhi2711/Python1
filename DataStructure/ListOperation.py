0f993373b6f6e7d43546843311d007cb6772bc56
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-26 19:40:25 +0530

    Update ListOperation.py

--- DataStructure/ListOperation.py
+++ DataStructure/ListOperation.py
@@ -1,1 +1,24 @@
 
+list=[10,20,30,40,50,60,70,809,90]
+print("original list")
+print(list)
+
+#append
+list.append(600)
+print("append 600")
+print(list)
+
+#insert
+list.insert(4,"sam")
+print("after adding sam.. at index 4")
+print(list)
+
+#extend
+list2=[20,30,40,50]
+list3=[1,2,3]
+list2.extend(list3)
+print("list2 extend list 3")
+print(list2)
+
+
+
