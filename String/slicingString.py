f8c0f6ca90bcc229da3a99803bea8c85303177e6
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-27 11:13:52 +0530

    Create slicingString.py

---
+++ String/slicingString.py
@@ -0,0 +1,27 @@
+#slicing string
+b = "Hello, World!"
+print(b[2:5])
+
+#slice from starting
+b = "Hello, World!"
+print(b[:5])
+
+#slice to the end
+b = "Hello, World!"
+print(b[2:])
+
+
+#negative indexing
+b = "Hello, World!"
+print(b[-5:-2])
+
+# Prints string in reverse
+b = "Hello, World!"
+print(b[::-1])
+
+# Extracting every second character "Hlo ol!"
+b = "Hello, World!"
+print(b[::2]) #Hlo ol
+
+
+
