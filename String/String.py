cbfed19692120d3ef78dd19638f75ce2941c3c80
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-27 10:40:55 +0530

    Update String.py

--- String/String.py
+++ String/String.py
@@ -1,2 +1,19 @@
 str="SAmrushi GAdge"
 print(str)
+
+#multiple string
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+#String Array
+a = "Hello, World!"
+print(a[1])
+print(a[3])
+
+#string using looping
+for x in "banana":
+  print(x)
+
