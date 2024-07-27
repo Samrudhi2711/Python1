8181f76c96683e1abd1598cb39bd10c68ff24e90
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-27 10:44:55 +0530

    Create MembershipOperator.py

---
+++ String/MembershipOperator.py
@@ -0,0 +1,8 @@
+#check free present in string
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#if not present in string
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
