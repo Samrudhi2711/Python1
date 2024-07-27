9302d2bb951dc1bb8978c60ad8a030a078fb81e3
Sam-Excelr <119813406+Samrudhi2711@users.noreply.github.com>
2024-07-27 11:47:01 +0530

    Update funArithmatic.py

--- Function/funArithmatic.py
+++ Function/funArithmatic.py
@@ -1,1 +1,10 @@
-
+def operation():
+    print("Enter 2 number:")
+    a=int(input())
+    b=int(input())
+    print("addition:",(a+b))
+    print("substraction:",(a-b))
+    print("multiplication:",(a*b))
+    print("division:",(a//b))
+    print("mod:",(a%b))
+operation()
